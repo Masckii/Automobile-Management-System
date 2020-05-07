f4cb3c1c1df7716b9de139ce9d87459fdc7de241
Samir Mohamed <36963951+samir162462@users.noreply.github.com>
2020-05-08 00:16:09 +0200

    Update Main.java

--- AMS/src/esper/Main.java
+++ AMS/src/esper/Main.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package esper;
+import model.AutoMobileManagmentSystem;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
@@ -15,11 +16,25 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    
+      final  model.AutoMobileManagmentSystem ams = new AutoMobileManagmentSystem();
     public static void main(String[] args) {
         // Disable logging
-        Logger.getRootLogger().setLevel(Level.OFF);
-
+        Logger.getRootLogger().setLevel(Level.OFF);         
         // Register events
         Config.registerEvents();
+        
+         // Create Kettle
+      
+
+        Config.createStatement("select value from Cruise_con_Reading")
+                .setSubscriber(new Object() {
+                    public void update(int temp) throws InterruptedException {
+                      //  ams.tempSignal(temp); expected feed back to be writen 
+                    }
+                });
+
+        
+        
     }
 }
