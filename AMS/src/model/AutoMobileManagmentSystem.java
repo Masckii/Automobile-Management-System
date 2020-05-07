5370021a76c2f84c4b871ecaa605ba9f40c5f063
Samir Mohamed <36963951+samir162462@users.noreply.github.com>
2020-05-07 22:37:16 +0200

    at=ms

--- AMS/src/model/AutoMobileManagmentSystem.java
+++ AMS/src/model/AutoMobileManagmentSystem.java
@@ -3,23 +3,40 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package model;
+
+import view.View_AMS;
 
 /**
  *
  * @author Omar Shan
  */
 public class AutoMobileManagmentSystem {
+
+
+
+    //--engine
+    private boolean state_engine = false;
+
+    //-- compunents of our RMS 
     private CruiseController cc;
     private Monitor monitor;
     private DriveShaftSensor dss;
-
+    private Model.Fuel_Sensor fuel_sensor;
+    private Model.Led_light led_light;
+    
+    
+    //gui
+    private view.View_AMS gui;
+    //-
+    
+    
     public AutoMobileManagmentSystem() {
         this.cc = new CruiseController();
         this.monitor = new Monitor();
         this.dss = new DriveShaftSensor();
+        gui = new View_AMS();
+
     }
-    
-    
+
 }
