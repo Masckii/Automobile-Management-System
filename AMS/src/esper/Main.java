df4807346ef8437ef720279975f249e41fce9389
Omar <Omar@Sikiri-PC>
2020-05-08 00:07:49 +0200

    Modifies Main Class

--- AMS/src/esper/Main.java
+++ AMS/src/esper/Main.java
@@ -16,6 +16,10 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Disable logging
+        Logger.getRootLogger().setLevel(Level.OFF);
+
+        // Register events
+        Config.registerEvents();
     }
 }
